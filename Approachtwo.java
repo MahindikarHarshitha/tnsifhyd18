d205d59f24163bf16fb449c762703363caf95a1b
MahindikarHarshitha <168471779+MahindikarHarshitha@users.noreply.github.com>
2024-05-12 00:02:13 +0530

    Add files via upload

---
+++ Approachtwo.java
@@ -0,0 +1,16 @@
+package com.si.approachtwo;
+
+public class Approachtwo {
+	int d=40;
+	static String e="harsh";
+	void display1()
+	{
+		System.out.println("this is approachtwo");
+	}
+	static String show1()
+	{
+		return "heyy";
+	}
+	
+
+}
