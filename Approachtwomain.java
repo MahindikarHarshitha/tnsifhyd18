5a674976ae5a643ce4a55901b22b1ff1d09b56e4
MahindikarHarshitha <168471779+MahindikarHarshitha@users.noreply.github.com>
2024-05-12 00:02:55 +0530

    Add files via upload

---
+++ Approachtwomain.java
@@ -0,0 +1,14 @@
+package com.si.approachtwo;
+
+public class Approachtwomain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          Approachtwo a2=new Approachtwo();
+          System.out.println(a2.d);
+          System.out.println(Approachtwo.e);
+          a2.display1();
+          
+	}
+
+}
